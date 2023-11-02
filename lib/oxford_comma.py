613244e0d2268ae24a7d4e1b9279d3a74675c856
Connor <connordbrown@gmail.com>
2023-11-02 08:04:53 -0600

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return "".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    elif len(items) == 3:
+        return ", ".join(items[0:2]) + ", and " + items[2]
+    else:
+        return ", ".join(items[0 : len(items)-1]) + ", and " + items[-1]
+    
